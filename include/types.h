14f86234cccf1db92de28b143797e0613902badf
Kha Tieu <tieuduykha1810@gmail.com>
2024-09-09 17:09:07 +0300

    adding code together

--- include/types.h
+++ include/types.h
@@ -1,14 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   types.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ktieu <ktieu@student.hive.fi>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/09 17:07:54 by ktieu             #+#    #+#             */
+/*   Updated: 2024/09/09 17:07:57 by ktieu            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 typedef enum e_token_type
 {
-	COMMAND,
-	PIPE,
-	OR,
-	AND,
-	REDIRECT_IN_O,
-	REDIRECT_OUT_O,
-	REDIRECT_IN_A,
-	REDIRECT_OUT_A,
+	CMD,					// command (ls, cat ...)
+	ARG,					// option (-l, -d ...) or file (infile.txt)
+	PIPE,					// |
+	OR,						// ||
+	AND,					// &&
+	SEMICOLON,				// ; (mkdir new; cd new; echo "successful")
+	BACKGROUND,				// & (sleep 10 & echo "running another function")
+	REDIRECT_IN,			// <
+	REDIRECT_OUT,			// >
+	REDIRECT_ERR,			// 2> Error Redirection 
+	REDIRECT_COMBINED,		// &> Combined Output Redirection
+	HERE_DOC,				// <<
+	APPEND					// >>
 }	t_token_type;
 
 typedef struct s_token
@@ -17,14 +33,13 @@ typedef struct s_token
 	char			*str;
 }	t_token;
 
-
-
 typedef struct s_shell
 {
 	char	**envp;
 	char	*cwd;
 	int		exited;
 	int		aborted;
+	t_token	*tokens;
 }	t_shell;
 
 
