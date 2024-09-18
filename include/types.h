6a1c9872ce84d0c8ab6e40883c266cc6455310fa
Hieu Trung Tran <hitran@c1r3p3.hive.fi>
2024-09-18 14:34:07 +0300

    changed types.h

--- include/types.h
+++ include/types.h
@@ -6,7 +6,7 @@
 /*   By: hitran <hitran@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/09 17:07:54 by ktieu             #+#    #+#             */
-/*   Updated: 2024/09/17 10:35:45 by hitran           ###   ########.fr       */
+/*   Updated: 2024/09/18 14:33:33 by hitran           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,15 +30,19 @@ typedef enum e_token_type
 {
 	CMD,					// command
 	PIPE,					// |
-	RD_IN,					// <
-	RD_OUT,					// >
-	RD_HEREDOC,				// <<
-	RD_APPEND,				// >>
 	OR,						// ||				#bonus
 	AND,					// &&				#bonus
 	BR_OPEN,				// ( 				#bonus
 	BR_CLOSE				// ) 				#bonus
 }	t_token_type;
+
+typedef enum e_redirecct_type
+{
+	RD_IN,					// <
+	RD_OUT,					// >
+	RD_HEREDOC,				// <<
+	RD_APPEND,				// >>
+}	t_redirect_type;
 
 typedef enum e_err_type
 {
@@ -51,21 +55,21 @@ typedef enum e_err_type
 // TOKEN 
 //-----------------------------------------------------
 
+typedef	struct s_redirect
+{
+	int					fd;
+	t_redirect_type 	type;
+	char				*path;
+	struct s_redirect	*next;
+}	t_redirect;
+
 typedef struct s_token
 {
 	t_token_type	type;
-	//command va redirect se luu chung thanh 1 array:
-	// - neu co RD thi loai la RD tuong ung, 
-	// - neu k co RD thi loai la CMD
-	// dieu nay de tranh truong hop chuoi lenh nhu sau: 
-	// "<input" ls luc nay "<input" duoc xem la command loi
-	// -> nhung sau khi parse thanh <input va ls thi <input lai thanh RD
-	char			**command; // sua thanh ** chua command va option + redirect (neu co)
-	//neu co redirect, chuyen no thanh command[0] voi dinh dang la RD viet lien voi path
-	// vi du: < "__path" grep hello  -> command[0] = <__path (_ la space); command[1] = grep; command[2] = hello
-	// khi execute: kiem tra command[0][0] == < || > || << || >> -> open(&command[0][1]) ->tang pointer command ->command[1]
-	// neu k co RD, giu nguyen
-	// goi exeve(command[0], command)
+	char			*cmd;
+	char			**split_cmd;
+	t_redirect		*redirect_in;
+	t_redirect		*redirect_out;
 }	t_token;
 
 typedef struct s_tokens
@@ -87,8 +91,7 @@ typedef struct s_tokens
  */
 typedef struct s_ast
 {
-	t_token_type	type;
-	char			*arg;
+	t_token			token;
 	struct s_ast	*left;
 	struct s_ast	*right;
 }	t_ast;
