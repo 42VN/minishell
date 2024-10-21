aab1c8828d5918d4275175038cb7cde96fac3304
tranhieutrung <tranhieutrung@outlook.com>
2024-10-21 20:57:46 +0300

    Added builin echo and pwd.

--- src/builtin/pwd.c
+++ src/builtin/pwd.c
@@ -1,30 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   echo.c                                             :+:      :+:    :+:   */
+/*   pwd.c                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: hitran <hitran@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/10/18 10:40:06 by hitran            #+#    #+#             */
-/*   Updated: 2024/10/21 09:46:44 by hitran           ###   ########.fr       */
+/*   Created: 2024/10/18 21:16:11 by hitran            #+#    #+#             */
+/*   Updated: 2024/10/21 09:47:16 by hitran           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	builtin_echo(char **token)
+void	builtin_pwd()
 {
-	int	index;
+	char	*cwd;
 
-	index = 1;
-	if (token[1] && !ft_strcmp(token[1], "-n"))
-		index++;
-	while (token[index])
-	{
-		printf("%s", token[index++]);
-		if (token[index] != NULL)
-			printf(" ");
-	}
-	if (token[1] && ft_strcmp(token[1], "-n"))
-		printf("\n");
+	cwd = getcwd(NULL, 0);
+	printf("%s\n", cwd);
+	free(cwd);
 }
