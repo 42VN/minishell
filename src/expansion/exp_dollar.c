52d8a26043e45acc9ab9b4d82150b8c1d8c98ff6
tieud1 <tieuduykha1810@gmail.com>
2024-11-08 10:56:46 +0200

    add helper funtion in exp_dollar.c

--- src/expansion/exp_dollar.c
+++ src/expansion/exp_dollar.c
@@ -6,15 +6,38 @@
 /*   By: ktieu <ktieu@student.hive.fi>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/05 14:16:00 by ktieu             #+#    #+#             */
-/*   Updated: 2024/11/05 14:57:18 by ktieu            ###   ########.fr       */
+/*   Updated: 2024/11/08 10:53:50 by ktieu            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-static char	*exp_handle_dollar_var(t_shell *shell, char *cmd, size_t *j)
+/**
+ * Expansion function searching for an env variable
+ */
+static char	*exp_dollar_get_var(t_shell *shell, char *cmd, size_t *j)
 {
-	
+	char	*exp_var;
+	char	*env_var_val;
+	char	res;
+	size_t	start;
+
+	start = *j;
+	while (exp_valid_dollar_var(cmd[*j]))
+		(*j)++;
+	exp_var = ft_substr(cmd, start, *j - start);
+	if (!exp_var)
+	{
+		shell->err_type = ERR_MALLOC;
+		return (NULL);
+	}
+	env_var_val = env_get(shell->envp, exp_var, 0);
+	free(exp_var);
+	if (!env_var_val)
+		res = ft_strdup("");
+	else
+		res = ft_strdup(env_var_val);
+	return (res);
 }
 
 void exp_dollar(
@@ -39,7 +62,7 @@ void exp_dollar(
 	else if (!exp_valid_dollar_var(cmd[*j]))
 		str = ft_strdup("$");
 	else
-		str = exp_handle_dollar_var(shell, cmd, j);
+		str = exp_dollar_get_var(shell, cmd, j);
 	if (!str)
 		return (ft_free_null(res));
 	joined = ft_strjoin(*res, str);
