61c0ca2d9379097efb22d7778a2097dbd3114e93
tieud1 <tieuduykha1810@gmail.com>
2024-11-27 13:54:29 +0200

    fixing quote handler in expansion

--- src/expansion/exp.c
+++ src/expansion/exp.c
@@ -3,40 +3,46 @@
 /*                                                        :::      ::::::::   */
 /*   exp.c                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hitran <hitran@student.hive.fi>            +#+  +:+       +#+        */
+/*   By: ktieu <ktieu@student.hive.fi>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/01 13:35:10 by ktieu             #+#    #+#             */
-/*   Updated: 2024/11/22 11:59:41 by hitran           ###   ########.fr       */
+/*   Updated: 2024/11/27 13:53:35 by ktieu            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-static int exp_post_process(t_shell *shell, char **res)
+static int exp_post_process(char **res, int i)
 {
-    size_t i;
-    char quote;
+	char	quote;
 
-	(void)shell; //check
+	quote = '\0';
     if (!res || !(*res))
-        return 0;
-    i = 0;
-    quote = '\0';
+    {
+		return (0);
+	}
     while ((*res)[i])
     {
         if ((*res)[i] == '\'' || (*res)[i] == '\"')
         {
             quote = (*res)[i];
-            break; 
+			break;
         }
         ++i;
     }
-    if (quote)
-    {
-        exp_strip_quotes(*res, quote);
-    }
-    return 1;
+	if (quote != '\0')
+	{
+		i = exp_strip_quotes(*res, quote);
+		// printf("Last quote: [%s] [%d] [%c]\n", *res, i, (*res)[i] ? (*res)[i] : '\0');
+		if (i >= 0 && (*res)[i] != '\0')
+		{
+			exp_post_process(res, i);
+		}
+	}
+    return (1);
 }
+
+
 
 static int	exp_process(t_shell *shell, char **res, char *cmd)
 {
@@ -72,7 +78,7 @@ static int	exp_logic(t_shell *shell, int i, int j)
 	exp_process(shell, &res, shell->tokens->array[i].split_cmd[j]);
 	if (!res)
 		return (ft_error_ret("expansion below: malloc", shell, ERR_MALLOC, 0));
-	exp_post_process(shell, &res);
+	exp_post_process(&res, 0);
 	ft_free_null(&shell->tokens->array[i].split_cmd[j]);
 	shell->tokens->array[i].split_cmd[j] = res;
 	return (1);
--- src/expansion/utils.c
+++ src/expansion/utils.c
@@ -6,7 +6,7 @@
 /*   By: ktieu <ktieu@student.hive.fi>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/05 14:01:21 by ktieu             #+#    #+#             */
-/*   Updated: 2024/11/22 12:48:44 by ktieu            ###   ########.fr       */
+/*   Updated: 2024/11/27 13:51:58 by ktieu            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,45 +47,41 @@ void	cleanup_split_cmd(char **split_cmd)
 	split_cmd[j] = NULL;
 }
 
-// static int find_op_quote(char *str)
-// {
-// 	while (*str)
-// 	{
-// 		if (*str == '\'' || *str == '\"')
-// 			return (*str);
-// 		str++;
-// 	}
-// 	return (0);
-// }
-//check
-
-void	exp_strip_quotes(char *str, char c)
+static void	exp_strip_quote_overwrite(char *str, char c, int *read, int *write)
 {
-	size_t	read;
-	size_t	write;
+	(*read)++;
+	while (str[*read] && str[*read] != c)
+		str[(*write)++] = str[(*read)++];
+}
+
+int	exp_strip_quotes(char *str, char c)
+{
+	int	read;
+	int	write;
+	int	last_quote;
 
 	read = 0;
 	write = 0;
-	if (!str || *str == '\0')
-        return;
-	if (c != '\'' && c != '\"')
-		return ;
+	last_quote = -1;
+	if (!str || *str == '\0' || (c != '\'' && c != '\"'))
+		return (0);
 	while (str[read])
 	{
 		if (str[read] == c)
 		{
-			read++;
-			while (str[read] && str[read] != c)
-				str[write++] = str[read++];
+			exp_strip_quote_overwrite(str, c, &read, &write);
 			if (str[read] == c)
+			{
+				last_quote = read;
 				read++;
+			}
 		}
 		else
-		{
 			str[write++] = str[read++];
-		}
 	}
 	str[write] = '\0';
+	return (last_quote == -1 ? write : last_quote);
+	// return (last_quote);
 }
 
 void ft_join_quote(char **res, char *quote)
