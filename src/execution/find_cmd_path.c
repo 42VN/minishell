b5efc83bb77b1448adff3a13c921ae45a397b572
tranhieutrung <tranhieutrung@outlook.com>
2024-11-29 13:36:21 +0200

    Updated find_cmd_path to handle the command '.'

--- src/execution/find_cmd_path.c
+++ src/execution/find_cmd_path.c
@@ -6,7 +6,7 @@
 /*   By: hitran <hitran@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/11 15:11:40 by hitran            #+#    #+#             */
-/*   Updated: 2024/11/29 11:47:59 by hitran           ###   ########.fr       */
+/*   Updated: 2024/11/29 13:34:32 by hitran           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -53,20 +53,33 @@ static char	*get_command_path(char **envp_paths, char *command)
 	return (NULL);
 }
 
-static void	check_command(t_shell *shell, char *command)
+static int	check_command(t_shell *shell, char *command)
 {
 	struct stat	sb;
 
-	if (stat(command, &sb) == 0 && S_ISDIR(sb.st_mode))
+	if (!command || !command[0] || !ft_strcmp(command, "."))
 	{
-		ft_printf_fd(STDERR_FILENO, "%s: Is a directory\n", command);
-		exec_error (shell, NULL);
+		if (!command || !command[0])
+			ft_printf_fd(STDERR_FILENO, "Command '%s' not found\n", command);
+		else if (!ft_strcmp(command, "."))
+			ft_printf_fd(STDERR_FILENO, "minishell: .: filename argument requi"
+				"red\n.: usage: . filename [arguments]\n");
+		return (EXIT_FAILURE);
 	}
-	else if (stat(command, &sb) == 0 && access(command, X_OK) != 0)
+	else if (ft_strchr(command, '/'))
 	{
-		ft_printf_fd(STDERR_FILENO, "%s: Permission denied\n", command);
-		exec_error (shell, NULL);
+		if (stat(command, &sb) == 0 && S_ISDIR(sb.st_mode))
+		{
+			ft_printf_fd(STDERR_FILENO, "%s: Is a directory\n", command);
+			exec_error (shell, NULL);
+		}
+		else if (stat(command, &sb) == 0 && access(command, X_OK) != 0)
+		{
+			ft_printf_fd(STDERR_FILENO, "%s: Permission denied\n", command);
+			exec_error (shell, NULL);
+		}
 	}
+	return (EXIT_SUCCESS);
 }
 
 char	*find_command_path(t_shell *shell, char *command)
@@ -74,14 +87,10 @@ char	*find_command_path(t_shell *shell, char *command)
 	char		**envp_paths;
 	char		*command_path;
 
-	if (!command || !command[0])
-	{
-		ft_printf_fd(STDERR_FILENO, "Command '%s' not found\n", command);
+	if (check_command(shell, command) == EXIT_FAILURE)
 		return (NULL);
-	}
 	if (ft_strchr(command, '/'))
 	{
-		check_command(shell, command);
 		if (access(command, F_OK) == 0)
 			return (ft_strdup(command));
 		ft_printf_fd(STDERR_FILENO, "%s: No such file or directory\n", command);
