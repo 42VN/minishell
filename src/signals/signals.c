9c6799e06202478b77f92163acb011e203c7030f
Kha Tieu <tieuduykha1810@gmail.com>
2024-12-17 15:42:02 +0200

    shorten code in signals.c

--- src/signals/signals.c
+++ src/signals/signals.c
@@ -6,65 +6,11 @@
 /*   By: ktieu <ktieu@student.hive.fi>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/08 12:58:02 by hitran            #+#    #+#             */
-/*   Updated: 2024/12/17 15:17:09 by ktieu            ###   ########.fr       */
+/*   Updated: 2024/12/17 15:40:00 by ktieu            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
-
-static int	set_parent(void)
-{
-	struct sigaction	sa;
-
-	rl_done = 0;
-	rl_event_hook = NULL;
-	sigemptyset(&sa.sa_mask);
-	sa.sa_handler = sig_parent_handler;
-	sa.sa_flags = SA_RESTART;
-	if (sigaction(SIGINT, &sa, NULL) == -1)
-		return (0);
-	if (signal(SIGQUIT, SIG_IGN) == SIG_ERR)
-		return (0);
-	return (1);
-}
-
-int	start_signal(t_shell *shell, t_signal_type type)
-{
-	set_signal_exit(shell);
-	shell->aborted = 1;
-	if (type == PARENT && !set_parent())
-		return (0);
-	else if (type == CHILD && (signal(SIGINT, SIG_DFL) == SIG_ERR
-			|| signal(SIGQUIT, SIG_DFL) == SIG_ERR))
-		return (0);
-	else if (type == HEREDOC)
-	{
-		rl_event_hook = event;
-		if (signal(SIGINT, sig_heredoc_handler) == SIG_ERR)
-			return (0);
-	}
-	shell->aborted = 0;
-	return (1);
-}
-
-
-//HOANG SIGNAL
-
-void	sigint_handler(int signum)
-{
-	(void)signum;
-	write(STDOUT_FILENO, "\n", 1);
-	rl_on_new_line();
-	rl_replace_line("", 0);
-	rl_redisplay();
-}
-
-void	sigint_handler_heredoc(int signum)
-{
-	(void)signum;
-	close(STDIN_FILENO);
-	// write(STDOUT_FILENO, "\n", 1);
-}
 
 int	reset_signals(void)
 {
@@ -98,8 +44,41 @@ int	set_signal_handler(int signum, void (*handler)(int))
 	if (sigaction(signum, &sa, NULL) < 0)
 	{
 		perror("minishell: sigaction");
-		return (-1);
+		return (0);
 	}
-	return (0);
+	return (1);
 }
 
+void	sigint_handler(int signum)
+{
+	(void)signum;
+	write(STDOUT_FILENO, "\n", 1);
+	rl_on_new_line();
+	rl_replace_line("", 0);
+	rl_redisplay();
+}
+
+static int	set_parent(void)
+{
+	set_signal_handler(SIGINT, sigint_handler);
+	set_signal_handler(SIGQUIT, SIG_IGN);
+	return (1);
+}
+
+int	start_signal(t_shell *shell, t_signal_type type)
+{
+	set_signal_exit(shell);
+	shell->aborted = 1;
+	if (type == PARENT && !set_parent())
+		return (0);
+	else if (type == CHILD && !reset_signals())
+		return (0);
+	else if (type == HEREDOC)
+	{
+		rl_event_hook = event;
+		if (signal(SIGINT, sig_heredoc_handler) == SIG_ERR)
+			return (0);
+	}
+	shell->aborted = 0;
+	return (1);
+}
