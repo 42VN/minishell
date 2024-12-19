a2f4f2c278249975bc7a923c439a69cc9f35bc4d
Hieu Trung Tran <hitran@c1r2p5.hive.fi>
2024-12-19 14:04:32 +0200

    Added condition to handle redirections are varaibles with white spaces.

--- src/execution/check_ambigous_redir.c
+++ src/execution/check_ambigous_redir.c
@@ -6,26 +6,39 @@
 /*   By: hitran <hitran@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/03 15:06:57 by hitran            #+#    #+#             */
-/*   Updated: 2024/12/19 12:34:26 by hitran           ###   ########.fr       */
+/*   Updated: 2024/12/19 14:01:36 by hitran           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+int	ft_is_all_white_spaces(char *s)
+{
+	while (*s)
+	{
+		if (*s != 32 && (*s < 9 || *s > 13))
+			return (0);
+		s++;
+	}
+	return (1);
+}
+
 int	check_ambigous_redir(t_shell *shell, t_redirect *redirect, int *fd)
 {
 	if (redirect->path && redirect->org_path)
 	{
-		if (!redirect->path[0] && redirect->org_path[0] 
+		if (!redirect->path[0] && redirect->org_path[0]
 			&& !inside_quotes(redirect->org_path)
 			&& redirect->type != RD_HEREDOC)
+			return (open_error(shell, redirect->org_path, fd,
+					"ambiguous redirect"));
+		else if (ft_strchr(redirect->org_path, '$') && ft_strchr(redirect->path, 32) && !ft_is_all_white_spaces(redirect->path))
 			return (open_error(shell, redirect->org_path, fd,
 					"ambiguous redirect"));
 		else if (!ft_strcmp(redirect->org_path, "*"))
 			return (open_error(shell, redirect->path, fd,
 					"ambiguous redirect"));
 		return (EXIT_SUCCESS);
-
 	}
 	return (EXIT_FAILURE);
 }
